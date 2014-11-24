a12822562b31c18f5e93cb49b4ab2cd59619603a
Varesh Tapadia <varesh.tapadia@gmail.com>
2014-11-24 23:26:46 +0100

    adding basic application class

---
+++ src/main/java/com/envyleague/cricket/Application.java
@@ -0,0 +1,40 @@
+package com.envyleague.cricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+/**
+ * Created by VTapadia on 24.11.2014.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+    /**
+     * Main method, used to run the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setShowBanner(false);
+
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+
+        // Check if the selected profile has been set as argument.
+        // if not the development profile will be added
+        addDefaultProfile(app, source);
+        app.run(args);
+    }
+
+    /**
+     * Set a default profile if it has not been set
+     */
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active")) {
+            app.setAdditionalProfiles("dev");
+        }
+    }
+
+}
