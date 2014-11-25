311c53ed2c552ec2bb320f03ec0f40c5e049490f
Varesh Tapadia <varesh.tapadia@gmail.com>
2014-11-26 00:10:51 +0100

    Still setting up env

--- src/main/java/com/envyleague/cricket/Application.java
+++ src/main/java/com/envyleague/cricket/Application.java
@@ -1,18 +1,39 @@
 package com.envyleague.cricket;
 
+import com.envyleague.cricket.config.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
-/**
- * Created by VTapadia on 24.11.2014.
- */
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Arrays;
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
+
+    private final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Inject
+    private Environment env;
+
+    @PostConstruct
+    public void initApplication() throws IOException {
+        if (env.getActiveProfiles().length == 0) {
+            log.warn("No Spring profile configured, running with default configuration");
+        } else {
+            log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+        }
+    }
+
     /**
      * Main method, used to run the application.
      */
@@ -33,7 +54,7 @@ public class Application {
      */
     private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
         if (!source.containsProperty("spring.profiles.active")) {
-            app.setAdditionalProfiles("dev");
+            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
         }
     }
 
--- src/main/java/com/envyleague/cricket/config/AppConfig.java
+++ src/main/java/com/envyleague/cricket/config/AppConfig.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Created by VTapadia on 25.11.2014.
- */
 @Configuration
 public class AppConfig {
     Logger log = LoggerFactory.getLogger(AppConfig.class);
