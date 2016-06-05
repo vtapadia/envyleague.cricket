a704b50814d32844990919b794733cdbef7e17d3
Varesh Tapadia <varesh.tapadia@gmail.com>
2016-06-05 21:46:31 +0200

    Configuring dev profile as default.

--- src/main/java/com/envyleague/cricket/config/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.envyleague.cricket.config;
-
-public interface Constants {
-    public String SPRING_PROFILE_DEVELOPMENT    = "dev";
-    public String SPRING_PROFILE_PRODUCTION     = "prod";
-    public String SPRING_PROFILE_HEROKU         = "heroku";
-}
